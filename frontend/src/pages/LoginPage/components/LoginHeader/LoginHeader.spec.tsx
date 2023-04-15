b6190afcee1d7f1a8a5256ed8863d67b0ad83134
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-15 02:03:25 -0300

    test: :white_check_mark: test component login header

---
+++ frontend/src/pages/LoginPage/components/LoginHeader/LoginHeader.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import LoginHeader from "./LoginHeader";
+
+describe("Test Login Header", () => {
+  it("Exibe a logo", async () => {
+    render(<LoginHeader />);
+
+    const result = screen.getByAltText(/logo contato seguro/i);
+
+    expect(result).toBeInTheDocument();
+  });
+});
