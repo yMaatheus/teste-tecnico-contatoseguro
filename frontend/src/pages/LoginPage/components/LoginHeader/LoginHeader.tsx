16a299bf667f4cad7096f6dea168ea69c7c6897b
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-15 01:39:15 -0300

    feat: :sparkles: create components login header

---
+++ frontend/src/pages/LoginPage/components/LoginHeader/LoginHeader.tsx
@@ -0,0 +1,9 @@
+import logo from "../../../../assets/images/logo-contato-seguro-white.png";
+
+const LoginHeader = () => (
+  <header className="p-4 pl-10">
+    <img src={logo} alt="Logo Contato Seguro" />
+  </header>
+);
+
+export default LoginHeader;
