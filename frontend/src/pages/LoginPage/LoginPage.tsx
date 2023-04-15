114508e5148cc3d52960623577d4838aa1b29016
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-15 01:39:53 -0300

    refactor: :recycle: add components login header and login form to login page

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,27 +1,25 @@
-export const LoginPage = () => (
-  <div>
-    <div>
-      <span>
-        <strong>Faça seu login</strong>
-        e seja
-        <br />
-        bem-vindo(a) à plataforma.
-      </span>
-      <form action="">
-        <label htmlFor="email">
-          <input type="text" id="email" name="email" placeholder="Login" />
-        </label>
-        <label htmlFor="password">
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-          />
-        </label>
-        <span>Esqueceu sua senha?</span>
-        <button type="button">Login</button>
-      </form>
+import LoginForm from "./components/LoginForm";
+import LoginHeader from "./components/LoginHeader";
+
+const LoginPage = () => (
+  <div className="h-full w-screen login-bg">
+    <div className="ml-auto mr-auto pt-10 grid h-screen w-8/12 grid-rows-[10%_60%] gap-10 ">
+      <LoginHeader />
+      <main className="flex justify-between">
+        <div className="self-end text-white w-[30rem]">
+          <p className="flex flex-col text-4xl text-center">
+            <span className="bg-blue-500 p-2 text-5xl font-bold rounded-lg">
+              Canal de Denúncias:
+            </span>
+            tecnologia que protege a integridade do seu negócio!
+          </p>
+        </div>
+        <div className="h-[80%] w-[30rem] rounded-xl bg-white self-end">
+          <LoginForm />
+        </div>
+      </main>
     </div>
   </div>
 );
+
+export default LoginPage;
