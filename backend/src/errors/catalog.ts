2c52ce44970521444aeccb277e30ea5cce2af756
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-16 15:48:08 -0300

    feat(Backend): :sparkles: create errors catalog to backend app

---
+++ backend/src/errors/catalog.ts
@@ -0,0 +1,41 @@
+import { StatusCodes } from "http-status-codes";
+
+type ErrorResponseObject = {
+  error: string;
+  httpStatus: number
+};
+
+export type ErrorCatalog = {
+  [key in ErrorTypes]: ErrorResponseObject
+};
+
+export enum ErrorTypes {
+  InvalidCredentials = 'InvalidCredentials',
+  TokenNotFound = 'TokenNotFound',
+  AllFieldsMustBeFilled = 'AllFieldsMustBeFilled',
+  InvalidId = 'InvalidId',
+  WrongPassword = 'WrongPassword',
+}
+
+export const errorCatalog: ErrorCatalog = {
+  InvalidCredentials: {
+    error: 'Invalid credentials',
+    httpStatus: 401,
+  },
+  TokenNotFound: {
+    error: 'Token not found',
+    httpStatus: 401,
+  },
+  AllFieldsMustBeFilled: {
+    error: 'All fields must be filled',
+    httpStatus: 400,
+  },
+  InvalidId: {
+    error: 'Id invalid',
+    httpStatus: 400,
+  },
+  WrongPassword: {
+    error: 'Wrong password',
+    httpStatus: StatusCodes.UNAUTHORIZED,
+  }
+};
