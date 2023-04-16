dc9be9e9118d93de3798a0a3b541818160b1824a
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-16 15:57:25 -0300

    feat(Backend): :sparkles: create routers company, user and report

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import authRouter from './auth.routes'
+import userRouter from './user.routes'
+import companyRouter from './company.routes'
+import reportRouter from './report.routes'
 
 export const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/user', userRouter);
+router.use('/company', companyRouter);
+router.use('/report', reportRouter);
