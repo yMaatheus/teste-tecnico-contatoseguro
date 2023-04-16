5ea9b8ae3701488f2fccd906b1c0e94beb99a326
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-16 02:15:51 -0300

    refactor(Frontend): :recycle: refactor routes manager component

---
+++ frontend/src/routes/Router.tsx
@@ -0,0 +1,28 @@
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import ErrorPage from "../pages/ErrorPage";
+import { ProtectedLayout } from "../layouts/ProtectedLayout";
+import LoginPage from "../pages/LoginPage";
+import UsersPage from "../pages/UsersPage";
+import { useAuth } from "../context/AuthProvider";
+
+export const Router = () => {
+  const { user } = useAuth();
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={user ? <Navigate to="/usuarios" replace /> : <LoginPage />}
+        />
+
+        <Route element={<ProtectedLayout />}>
+          <Route path="usuarios" element={<UsersPage />} />
+          <Route path="empresas" element={<h1>Empresas</h1>} />
+          <Route path="relatorios" element={<h1>Relatorio</h1>} />
+        </Route>
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
