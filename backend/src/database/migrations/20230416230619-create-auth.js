06fe480bb6d8933914d3b65804de9dd6b4579a2e
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-16 20:26:02 -0300

    feat(Backend): :sparkles: create migration create auth

---
+++ backend/src/database/migrations/20230416230619-create-auth.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('auth', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at'
+      }
+    })
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('auth');
+  }
+};
