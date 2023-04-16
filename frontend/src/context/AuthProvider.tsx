90612a02770a46d760d0eb0f738b434cbeb61960
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-16 02:12:44 -0300

    refactor(Frontend): :recycle: refactor code to auth provider

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -1,26 +1,33 @@
 import React, { useState } from "react";
 import { createContext } from "react";
-import Cookies from "js-cookie";
+import { loginUser, logoutUser } from "../services/user";
+import { clearUser, getUser, setUserLocal } from "../utils/localStorage.util";
 
 type AuthContextType = {
   user: string | null | undefined;
-  signout: (callback: VoidFunction) => void;
+  signout: () => void;
+  signIn: (email: string, password: string) => Promise<void>;
 };
 
 const AuthContext = createContext({} as AuthContextType);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<string | null | undefined>(
-    Cookies.get("user")
-  );
+  const [user, setUser] = useState<string | null | undefined>(getUser());
 
-  const signout = (callback: VoidFunction) => {
-    Cookies.remove("user");
-    setUser(null);
-    callback();
+  const signIn = async (email: string, password: string) => {
+    const data = await loginUser(email, password);
+
+    setUser(data);
+    setUserLocal(data);
   };
 
-  const value = { user, signout };
+  const signout = async () => {
+    await logoutUser();
+    setUser(null);
+    clearUser();
+  };
+
+  const value = { user, signout, signIn };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
