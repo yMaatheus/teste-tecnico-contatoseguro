4811a51fafd3589cdbc5014cdedf184c0844ef0f
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-16 00:31:41 -0300

    build: :heavy_plus_sign: setup cors cookie parser and routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,28 @@
+import cookieParser from 'cookie-parser';
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import { StatusCodes } from 'http-status-codes';
+import { router } from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: FRONTEND_URL,
+  credentials: true,
+}));
+app.use(cookieParser());
 
 app.get('/', (_req: Request, res: Response) => {
   res.status(StatusCodes.OK).send('Express + TypeScript')
 });
+
+app.use(router);
 
 export default app;
