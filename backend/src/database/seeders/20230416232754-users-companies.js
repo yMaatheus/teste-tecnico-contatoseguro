53b54e7b792f97e0b88f9e829dfc6c4b34347113
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-16 22:17:34 -0300

    refactor(Backend): :recycle: remove created_at and updated_at to seed users companies

--- backend/src/database/seeders/20230416232754-users-companies.js
+++ backend/src/database/seeders/20230416232754-users-companies.js
@@ -7,69 +7,47 @@ module.exports = {
     [
       {
         user_id: 1,
-        company_id: 2,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 2
       },
       {
         user_id: 1,
-        company_id: 3,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 3
       },
       {
         user_id: 1,
-        company_id: 4,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 4
       },
       {
         user_id: 2,
-        company_id: 1,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 1
       },
       {
         user_id: 2,
-        company_id: 3,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 3
       },
       {
         user_id: 3,
-        company_id: 5,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 5
       },
       {
         user_id: 4,
-        company_id: 3,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 3
       },
       {
         user_id: 4,
-        company_id: 4,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 4
       },
       {
         user_id: 4,
-        company_id: 5,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 5
       },
       {
         user_id: 5,
-        company_id: 5,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 5
       },
       {
         user_id: 6,
-        company_id: 1,
-        'created_at': Sequelize.literal('CURRENT_TIMESTAMP'),
-        'updated_at': Sequelize.literal('CURRENT_TIMESTAMP'),
+        company_id: 1
       },
     ], { });
   },
