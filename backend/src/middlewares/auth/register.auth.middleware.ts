9fa91f73e5644e3ccc6f50d7f70dc07d4f53b937
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-17 12:03:38 -0300

    feat(Backend): :sparkles: create register auth middleware and add to router

---
+++ backend/src/middlewares/auth/register.auth.middleware.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from 'express';
+import { AuthUserZodSchema } from '../../interfaces/IAuthUser';
+
+const registerAuthMiddleware = (req: Request, _res: Response, next: NextFunction) => {
+  const parsed = AuthUserZodSchema.safeParse(req.body);
+  if (!parsed.success) throw parsed.error;
+  return next();
+};
+
+export default registerAuthMiddleware;
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import authMiddleware from '../middlewares/auth.middleware';
 import AuthController from '../controllers/auth.controller';
 import AuthService from '../services/auth.service';
+import registerAuthMiddleware from '../middlewares/auth/register.auth.middleware';
 
 const authService = new AuthService();
 const authController = new AuthController(authService);
 
 const router = Router();
 
-router.post('/register', authController.register);
+router.post('/register', registerAuthMiddleware, authController.register);
 
 router.post('/login', authController.login);
 
