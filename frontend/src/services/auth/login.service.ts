126d737f32334e038a2538ab4817ec8528257cd3
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-17 05:10:34 -0300

    refactor(Frontend): :truck: rename user service to auth service

--- frontend/src/services/auth/login.service.ts
+++ frontend/src/services/auth/login.service.ts
@@ -1,7 +1,7 @@
 import api from "../../lib/axios";
 
 export const loginUser = async (email: string, password: string) => {
-  const { data } = await api.post("/user/login", { email, password });
+  const { data } = await api.post("/auth/login", { email, password });
 
   return data;
 };
