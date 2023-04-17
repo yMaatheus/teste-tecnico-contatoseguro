a3e48518f5ec4feb0e7be2fcef91d277451d6cca
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-17 12:38:49 -0300

    feat(Backend): :sparkles: add report validation middleware to router report

--- backend/src/routes/report.routes.ts
+++ backend/src/routes/report.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import ReportController from '../controllers/report.controller';
 import ReportService from '../services/report.service';
+import { createReportMiddleware } from '../middlewares';
 
 const reportService = new ReportService();
 const reportController = new ReportController(reportService);
@@ -8,7 +9,7 @@ const reportController = new ReportController(reportService);
 const router = Router();
 
 router.route('/')
-  .post(reportController.create)
+  .post(createReportMiddleware, reportController.create)
   .get(reportController.getAll)
 
 router.route('/:id')
