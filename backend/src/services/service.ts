22ab464cdc0ee48de28618c3fcdca528b952f287
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-18 02:07:43 -0300

    feat(Backend): :sparkles: create generic service

---
+++ backend/src/services/service.ts
@@ -0,0 +1,33 @@
+import { Model, ModelStatic } from "sequelize";
+import { ErrorTypes } from "../errors/catalog";
+import { IService } from "../interfaces/IService";
+
+export default class Service<T extends Model<T>> implements IService<T> {
+
+  constructor(public _model: ModelStatic<any>) { }
+
+  async create(body: Partial<T>) {
+    const result = await this._model.create(body);
+    return result.get();
+  }
+
+  async getAll() {
+    const result = await this._model.findAll();
+    return result;
+  }
+
+  async getById(id: number) {
+    const result = await this._model.findByPk(id);
+    return result;
+  }
+
+  async updateById(id: number, update: Partial<T>) {
+    const result = await this._model.update(update, { where: { id } });
+    if (!result) throw Error(ErrorTypes.EntityNotFound);
+  }
+
+  async deleteById(id: number) {
+    const result = await this._model.destroy({ where: { id } });
+    if (!result) throw Error(ErrorTypes.EntityNotFound);
+  }
+}
