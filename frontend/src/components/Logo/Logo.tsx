bba8c5ea37fd091b5937beeb780c79952cc36995
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-19 02:09:00 -0300

    feat(Frontend): :sparkles: add class wrapper to logo component

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -1,12 +1,13 @@
 import logo from "../../assets/images/logo.png";
 import PropTypes from "prop-types";
 
-export const Logo = ({ className = "" }) => (
-  <div>
+export const Logo = ({ className = "", classWrapper = "" }) => (
+  <div className={classWrapper}>
     <img className={className} src={logo} alt="Logo Contato seguro" />
   </div>
 );
 
 Logo.propTypes = {
   className: PropTypes.string,
+  classWrapper: PropTypes.string,
 };
