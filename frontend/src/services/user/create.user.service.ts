c99ffba1b41c266aebd97ae19e13bbe784dce2b8
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-19 17:54:26 -0300

    fix(Frontend): :bug: adjust endpoint post user

--- frontend/src/services/user/create.user.service.ts
+++ frontend/src/services/user/create.user.service.ts
@@ -2,7 +2,7 @@ import api from "../../lib/axios";
 import { UserType } from "../../types";
 
 export const createUser = async (user: Partial<UserType>) => {
-  const { data } = await api.post("/users", user);
+  const { data } = await api.post("/user", user);
 
   return data;
 };
