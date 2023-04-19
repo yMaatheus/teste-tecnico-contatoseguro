94a4fc365233aa68f9b5a1313dfc2a24a65683be
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-19 17:13:58 -0300

    feat(Frontend): :sparkles: implements design error page

--- frontend/src/pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,6 +1,10 @@
 export const ErrorPage = () => (
-  <div>
-    <h1>Ops!</h1>
-    <h2>PÁGINA NÃO ENCONTRADA</h2>
+  <div className="flex flex-col w-full h-screen bg-slate-400">
+    <main className="w-full h-full flex flex-col justify-center items-center gap-10">
+      <h1 className="text-9xl font-bold text-black">404</h1>
+      <span className="text-xl font-bold text-black">
+        PÁGINA NÃO ENCONTRADA
+      </span>
+    </main>
   </div>
 );
