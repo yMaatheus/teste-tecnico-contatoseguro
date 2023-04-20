9fdf6aae4e54de7d8256ad5030f745185a472b4b
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-20 04:24:51 -0300

    feat(Frontend): :sparkles: implements recive search to props of form component

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -1,49 +1,50 @@
 import React from "react";
 import { Logo } from "../Logo";
-import useAppStore from "../../lib/store";
 
 type FormType = {
   columns: string[];
   insertButton?: React.ReactNode;
   table?: React.ReactNode;
+  search: string;
+  searchLabel: string;
+  setSearch: (value: string) => void;
+  setSearchLabel: (value: string) => void;
 };
 
-export const Form = ({ columns, insertButton, table }: FormType) => {
-  const [search, setSearch, searchLabel, setSearchLabel] = useAppStore(
-    (state) => [
-      state.search,
-      state.setSearch,
-      state.searchLabel,
-      state.setSearchLabel,
-    ]
-  );
-  return (
-    <div className="flex flex-col gap-10 bg-slate-400 p-10 m-10">
-      <Logo className="h-24" classWrapper="flex justify-center" />
+export const Form = ({
+  columns,
+  insertButton,
+  table,
+  search,
+  searchLabel,
+  setSearch,
+  setSearchLabel,
+}: FormType) => (
+  <div className="flex flex-col gap-10 bg-slate-400 p-10 m-10">
+    <Logo className="h-24" classWrapper="flex justify-center" />
 
-      <div className="header-wrapper flex gap-10 justify-between">
-        <div>{insertButton}</div>
-        <div className="flex gap-9">
-          <input
-            type="text"
-            placeholder="Buscar..."
-            className="input w-full max-w-xs"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <select
-            className="select max-w-xs"
-            value={searchLabel}
-            onChange={(e) => setSearchLabel(e.target.value)}
-          >
-            {columns?.map((column) => (
-              <option key={`${column}`}>{column}</option>
-            ))}
-          </select>
-        </div>
+    <div className="header-wrapper flex gap-10 justify-between">
+      <div>{insertButton}</div>
+      <div className="flex gap-9">
+        <input
+          type="text"
+          placeholder="Buscar..."
+          className="input w-full max-w-xs"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <select
+          className="select max-w-xs"
+          value={searchLabel}
+          onChange={(e) => setSearchLabel(e.target.value)}
+        >
+          {columns?.map((column) => (
+            <option key={`${column}`}>{column}</option>
+          ))}
+        </select>
       </div>
-
-      {table}
     </div>
-  );
-};
+
+    {table}
+  </div>
+);
