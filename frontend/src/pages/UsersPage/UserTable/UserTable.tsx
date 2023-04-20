53db11b7f1ceceee94ce838e32d7ec7288fdc2b0
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-20 00:28:48 -0300

    feat(Frontend): implements delete user to user table component

--- frontend/src/pages/UsersPage/UserTable/UserTable.tsx
+++ frontend/src/pages/UsersPage/UserTable/UserTable.tsx
@@ -4,6 +4,7 @@ import { UserType } from "../../../types";
 import { UserEditModal } from "../UserEditModal";
 import { QueryObserverResult } from "@tanstack/react-query";
 import { UserDetailModal } from "../UserDetailModal";
+import { deleteUser } from "../../../services/user";
 
 export const UserTable = ({
   users = [],
@@ -11,39 +12,51 @@ export const UserTable = ({
 }: {
   users: UserType[];
   refetch: () => Promise<QueryObserverResult<UserType[], unknown>>;
-}) => (
-  <>
-    <GenericTable
-      keyExtractor={({ email }) => `${email}`}
-      data={users}
-      columns={
-        <tr>
-          <th>Nome</th>
-          <th>Email</th>
-          <th>Telefone</th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      }
-      renderItem={(user: UserType) => (
-        <>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>{user.phone}</td>
-          <td></td>
-          <td>
-            <UserDetailModal {...user} />
-          </td>
-          <td>
-            <UserEditModal {...user} refetch={refetch} />
-          </td>
-          <td>
-            <Button className="btn btn-ghost btn-xs">Remover</Button>
-          </td>
-        </>
-      )}
-    />
-  </>
-);
+}) => {
+  const handleDelete = (id: number) => {
+    deleteUser(id).then(() => refetch());
+  };
+  return (
+    <>
+      <GenericTable
+        keyExtractor={({ id }) => `${id}`}
+        data={users}
+        columns={
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Telefone</th>
+            <th>Cidade</th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        }
+        renderItem={(user: UserType) => (
+          <>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.cityOfBirth}</td>
+            <td></td>
+            <td>
+              <UserDetailModal {...user} />
+            </td>
+            <td>
+              <UserEditModal {...user} refetch={refetch} />
+            </td>
+            <td>
+              <Button
+                className="btn btn-ghost btn-xs"
+                onClick={() => handleDelete(user.id)}
+              >
+                Remover
+              </Button>
+            </td>
+          </>
+        )}
+      />
+    </>
+  );
+};
