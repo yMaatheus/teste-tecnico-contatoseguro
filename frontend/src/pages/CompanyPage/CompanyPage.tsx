823a396e37880d56b850f0f1cf0d89cfd647bc97
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-20 01:40:08 -0300

    feat(Frontend): :sparkles: implements search to company page

--- frontend/src/pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/pages/CompanyPage/CompanyPage.tsx
@@ -2,6 +2,8 @@ import { Form } from "../../components/Form";
 import { useQuery } from "@tanstack/react-query";
 import { getCompanies } from "../../services/company/getAll.company.service";
 import { CompanyTable } from "./CompanyTable";
+import { CompanyCreateModal } from "./CompanyCreateModal";
+import useAppStore from "../../lib/store";
 
 export const CompaniesPage = () => {
   const { data, refetch, isLoading } = useQuery({
@@ -9,17 +11,34 @@ export const CompaniesPage = () => {
     queryFn: getCompanies,
   });
 
-  const companies = data;
+  const [search, searchLabel] = useAppStore((state) => [
+    state.search,
+    state.searchLabel,
+  ]);
 
+  const companies = data //TODO - Refactor this
+    ?.filter((company) => {
+      if (searchLabel === "Nome") {
+        return company.name.toLowerCase().includes(search.toLowerCase());
+      } else if (searchLabel === "Endereço") {
+        return company.address.toLowerCase().includes(search.toLowerCase());
+      } else if (searchLabel === "Cidade") {
+        return company.city.toLowerCase().includes(search.toLowerCase());
+      } else if (searchLabel === "Estado") {
+        return company.state?.toLowerCase().includes(search.toLowerCase());
+      }
+      return true;
+    });
+
+  const table = !isLoading && (
+    <CompanyTable companies={companies || []} refetch={refetch} />
+  );
   return (
     <div className="w-full h-full flex justify-center items-center">
       <Form
         columns={["Nome", "Endereço", "Cidade", "Estado"]}
-        table={
-          !isLoading && (
-            <CompanyTable companies={companies || []} refetch={refetch} />
-          )
-        }
+        insertButton={<CompanyCreateModal refetch={refetch} />}
+        table={table}
       />
     </div>
   );
