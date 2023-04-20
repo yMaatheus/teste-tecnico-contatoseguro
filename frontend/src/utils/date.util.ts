0c14a418674519e3dc2be0f02d840898e7a51609
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-20 04:23:49 -0300

    refactor(Frontend): :recycle: refactor date utils

--- frontend/src/utils/date.util.ts
+++ frontend/src/utils/date.util.ts
@@ -1,10 +1,12 @@
 import dayjs from "dayjs";
 
-export const dateToView = (date = "") => {
+export const dateToView = (date: string | undefined | null) => {
   const result = dayjs(date).format("DD/MM/YYYY");
 
   if (dayjs(result).isValid()) return result;
 };
 
-export const ViewToDate = (date: string): string =>
-  dayjs(date, "DD/MM/YYYY").format("YYYY-MM-DD");
+export const ViewToDate = (date: string | undefined | null) => {
+  const result = dayjs(date, "DD/MM/YYYY").format("YYYY-MM-DD");
+  if (dayjs(result).isValid()) return result;
+};
